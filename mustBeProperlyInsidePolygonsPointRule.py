6e40888ed29a47b720475f523a1c7db658a76d69
Mauro Carlevaro <mauroctecno@gmail.com>
2019-08-04 22:50:06 -0300

    Improvement implementation in rule.py

--- mustBeProperlyInsidePolygonsPointRule.py
+++ mustBeProperlyInsidePolygonsPointRule.py
@@ -3,6 +3,7 @@
 import gvsig
 import sys
 
+from gvsig import geom
 from gvsig import uselib
 uselib.use_plugin("org.gvsig.topology.app.mainplugin")
 
@@ -22,79 +23,94 @@ class MustBeProperlyInsidePolygonsPointRule(AbstractTopologyRule):
         AbstractTopologyRule.__init__(self, plan, factory, tolerance, dataSet1, dataSet2)
         self.addAction(DeletePointAction())
     
-    def check(self, taskStatus, report, feature1):
-        try:
-            store2 = self.getDataSet2().getFeatureStore()
+    def intersects(self, buffer1, theDataSet2):
+        if theDataSet2.getSpatialIndex() != None:
+            result = False
+            for featureReference in theDataSet2.query(buffer1):
+                feature2 = featureReference.getFeature()
+                polygon2 = feature2.getDefaultGeometry()
+                if polygon2.contains( buffer1 ): #if buffer1.intersects( polygon2 ):
+                    result = True
+                    break
+        else:
             if self.expression == None:
                 manager = ExpressionEvaluatorLocator.getManager()
                 self.expression = manager.createExpression()
                 self.expressionBuilder = manager.createExpressionBuilder()
+                store2 = theDataSet2.getFeatureStore()
                 self.geomName = store2.getDefaultFeatureType().getDefaultGeometryAttributeName()
+            self.expression.setPhrase(
+                self.expressionBuilder.ifnull(
+                    self.expressionBuilder.column(self.geomName),
+                    self.expressionBuilder.constant(False),
+                    self.expressionBuilder.ST_Intersects(
+                        self.expressionBuilder.geometry(buffer1),
+                        self.expressionBuilder.column(self.geomName)
+                    )
+                ).toString()
+            )
+            if theDataSet2.findFirst(self.expression) == None:
+                result = False
+            else:
+                result = True
+        return result
+    
+    def check(self, taskStatus, report, feature1):
+        try:
             point1 = feature1.getDefaultGeometry()
             tolerance1 = self.getTolerance()
             theDataSet2 = self.getDataSet2()
-            if theDataSet2.getSpatialIndex() != None:
-                if point1.getGeometryType().getName() == "Point2D":
+            geometryType1 = point1.getGeometryType()
+            if geometryType1.getSubType() == geom.D2 or geometryType1.getSubType() == geom.D2M:
+                if geometryType1.getType() == geom.POINT:
                     buffer1 = point1.buffer(tolerance1)
-                    contains = False
-                    for featureReference in theDataSet2.query(buffer1):
-                        feature2 = featureReference.getFeature()
-                        polygon2 = feature2.getDefaultGeometry()
-                        if polygon2.getGeometryType().getName() == "Polygon2D":
-                            if polygon2.contains( buffer1 ): #if buffer1.intersects( polygon2 ):
-                                contains = True
-                                break
-                        else:
-                            if polygon2.getGeometryType().getName() == "MultiPolygon2D":
-                                n2 = polygon2.getPrimitivesNumber()
-                                for i in range(0, n2 + 1):
-                                    surface2 = polygon2.getSurfaceAt(i)
-                                    if surface2.contains( buffer1 ):#if buffer1.intersects( surface2 ):
-                                        contains = True
-                                        break
-                    if not contains:
+                    if not self.intersects(buffer1, theDataSet2):
                         report.addLine(self,
-                                    self.getDataSet1(),
-                                    self.getDataSet2(),
-                                    point1,
-                                    point1,
-                                    feature1.getReference(), 
-                                    None,
-                                    False,
-                                    "The point is not contained by polygon."
+                            self.getDataSet1(),
+                            self.getDataSet2(),
+                            point1,
+                            point1,
+                            feature1.getReference(),
+                            None,
+                            -1,
+                            -1,
+                            False,
+                            "The point is not contained by polygon.",
+                            ""
                         )
                 else:
-                    if point1.getGeometryType().getName() == "MultiPoint2D":
+                    if geometryType1.getType() == geom.MULTIPOINT:
                         n1 = point1.getPrimitivesNumber()
                         for i in range(0, n1 + 1):
                             buffer1 = point1.getPointAt(i).buffer(tolerance1)
-                            contains = False
-                            for featureReference in theDataSet2.query(buffer1):
-                                feature2 = featureReference.getFeature()
-                                polygon2 = feature2.getDefaultGeometry()
-                                if polygon2.getGeometryType().getName() == "Polygon2D":
-                                    if polygon2.contains( buffer1 ): #if buffer1.intersects( polygon2 ):
-                                        contains = True
-                                        break
-                                else:
-                                    if polygon2.getGeometryType().getName() == "MultiPolygon2D":
-                                        n2 = polygon2.getPrimitivesNumber()
-                                        for j in range(0, n2 + 1):
-                                            surface2 = polygon2.getSurfaceAt(j)
-                                            if surface2.contains( buffer1 ): #if buffer1.intersects( surface2 ):
-                                                contains = True
-                                                break
-                            if not contains:
+                            if not self.intersects(buffer1, theDataSet2):
                                 report.addLine(self,
-                                            self.getDataSet1(),
-                                            self.getDataSet2(),
-                                            point1.getPointAt(i),
-                                            point1.getPointAt(i),
-                                            feature1.getReference(), 
-                                            None,
-                                            False,
-                                            "The multipoint is not contained by polygon."
+                                    self.getDataSet1(),
+                                    self.getDataSet2(),
+                                    point1,
+                                    point1.getPointAt(i),
+                                    feature1.getReference(), 
+                                    None,
+                                    i,
+                                    -1,
+                                    False,
+                                    "The multipoint is not contained by polygon.",
+                                    ""
                                 )
+            else:
+                report.addLine(self,
+                    self.getDataSet1(),
+                    self.getDataSet2(),
+                    point1,
+                    point1,
+                    feature1.getReference(),
+                    None,
+                    -1,
+                    -1,
+                    False,
+                    "Unsupported geometry subtype.",
+                    ""
+                )
         except:
             ex = sys.exc_info()[1]
             gvsig.logger("Can't execute rule. Class Name: " + ex.__class__.__name__ + ". Exception: " + str(ex), gvsig.LOGGER_ERROR)
